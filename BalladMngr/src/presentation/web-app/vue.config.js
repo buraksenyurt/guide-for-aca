5227476cfff2d2145d9ceb34628173846b6eab18
buraksenyurt <burakselimsenyurt@gmail.com>
2022-09-05 18:24:10 +0300

    Last touchs.

--- BalladMngr/src/presentation/web-app/vue.config.js
+++ BalladMngr/src/presentation/web-app/vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  devServer: {
-    proxy: "https://localhost:7008/api",
-  },
 });
